a35b6c8ba50c8ae01bdca86f4e3b59a43f76c706
tliebeck <tliebeck@7bbdd6fe-0337-0410-95c6-f1e3ec963e97>
2009-03-11 11:30:49 +0000

    Tab removals.

--- src/webcontainer/java/nextapp/echo/filetransfer/webcontainer/sync/component/DownloadCommandPeer.java
+++ src/webcontainer/java/nextapp/echo/filetransfer/webcontainer/sync/component/DownloadCommandPeer.java
@@ -22,57 +22,57 @@ import nextapp.echo.webcontainer.service.JavaScriptService;
  */
 public class DownloadCommandPeer extends AbstractCommandSynchronizePeer {
 
-	private static final Map ID_TO_DOWNLOAD_MAP = Collections.synchronizedMap(new HashMap());
+    private static final Map ID_TO_DOWNLOAD_MAP = Collections.synchronizedMap(new HashMap());
 
-	private static final Service DOWNLOAD_SERVICE = JavaScriptService.forResource("Echo.Download",
-			"nextapp/echo/filetransfer/webcontainer/resource/js/Download.js");
+    private static final Service DOWNLOAD_SERVICE = JavaScriptService.forResource("Echo.Download",
+            "nextapp/echo/filetransfer/webcontainer/resource/js/Download.js");
 
-	static {
-		DownloadService.install();
-		WebContainerServlet.getServiceRegistry().add(DOWNLOAD_SERVICE);
-	}
+    static {
+        DownloadService.install();
+        WebContainerServlet.getServiceRegistry().add(DOWNLOAD_SERVICE);
+    }
 
-	public DownloadCommandPeer() {
-		super();
-		addProperty("uri", new AbstractCommandSynchronizePeer.PropertyPeer() {
-			public Object getProperty(Context context, Command command) {
-				DownloadCommand download = (DownloadCommand) command;
-				if (download.isActive()) {
-					download.setActive(false);
-				}
-				UserInstance userInstance = (UserInstance) context.get(UserInstance.class);
-				String id = download.getRenderId();
-				ID_TO_DOWNLOAD_MAP.put(id, download);
-				String serviceUri = DownloadService.getInstance().createUri(userInstance, id);
-				return serviceUri;
-			}
-		});
-	}
+    public DownloadCommandPeer() {
+        super();
+        addProperty("uri", new AbstractCommandSynchronizePeer.PropertyPeer() {
+            public Object getProperty(Context context, Command command) {
+                DownloadCommand download = (DownloadCommand) command;
+                if (download.isActive()) {
+                    download.setActive(false);
+                }
+                UserInstance userInstance = (UserInstance) context.get(UserInstance.class);
+                String id = download.getRenderId();
+                ID_TO_DOWNLOAD_MAP.put(id, download);
+                String serviceUri = DownloadService.getInstance().createUri(userInstance, id);
+                return serviceUri;
+            }
+        });
+    }
 
-	public void init(Context context) {
-		super.init(context);
-		ServerMessage serverMessage = (ServerMessage) context.get(ServerMessage.class);
-		serverMessage.addLibrary(DOWNLOAD_SERVICE.getId());
-	}
+    public void init(Context context) {
+        super.init(context);
+        ServerMessage serverMessage = (ServerMessage) context.get(ServerMessage.class);
+        serverMessage.addLibrary(DOWNLOAD_SERVICE.getId());
+    }
 
-	public Class getCommandClass() {
-		return DownloadCommand.class;
-	}
+    public Class getCommandClass() {
+        return DownloadCommand.class;
+    }
 
-	/**
-	 * Returns the {@link DownloadCommand} having the passed id, and removes
-	 * it from the internal map.
-	 * <p>
-	 * This means that a particular download command cannot be re-used. A new
-	 * download command must be created every time, e.g. each time your download
-	 * button is clicked.
-	 * </p>
-	 * This is necessary to prevent memory leaks.
-	 * 
-	 * @param id the download id.
-	 * @return the {@link DownloadCommand} instance.
-	 */
-	public static DownloadCommand getAndRemoveDownload(String id) {
-		return (DownloadCommand) ID_TO_DOWNLOAD_MAP.remove(id);
-	}
+    /**
+     * Returns the {@link DownloadCommand} having the passed id, and removes
+     * it from the internal map.
+     * <p>
+     * This means that a particular download command cannot be re-used. A new
+     * download command must be created every time, e.g. each time your download
+     * button is clicked.
+     * </p>
+     * This is necessary to prevent memory leaks.
+     * 
+     * @param id the download id.
+     * @return the {@link DownloadCommand} instance.
+     */
+    public static DownloadCommand getAndRemoveDownload(String id) {
+        return (DownloadCommand) ID_TO_DOWNLOAD_MAP.remove(id);
+    }
 }
